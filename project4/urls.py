787139d277bff23cac34894e55b97b06ecbe9ad0
Your Nameravi <ravipara143@gmail.com>
2021-08-13 14:31:10 +0530

    first commit

--- project4/urls.py
+++ project4/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import *
+from app2.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ravi/',ravi,name='ravi'),
+    path('ravi1/',ravi1,name='ravi1'),
 ]
